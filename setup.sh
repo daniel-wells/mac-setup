c8331fe1bd63b31f383b0546bed130f489a9bd48
daniel-wells <daniel-wells@users.noreply.github.com>
2017-02-19 23:06:33 +0000

    fix ssh & bash_profile symlinks

--- setup.sh
+++ setup.sh
@@ -10,15 +10,15 @@ scutil --get HostName
 
 xcode-select --install
 
+# Symlink bash profile
+ln -sfv ~/Dropbox/Github/mac-setup/bash_profile.sh ~/.bash_profile
+
 softwareupdate --install --all
 
 sh install-software.sh
 
 # after install dockutil
 sh osxdefaults.sh
-
-# Symlink bash profile
-ln -sfv "bash_profile.sh" ~/.bash_profile
 
 # Install Sublime Text settings
 cp -r Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*/Packages/User/Preferences.sublime-settings 2> /dev/null
@@ -29,6 +29,6 @@ cp -r Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text*
 
 # Instead manually use plist settings in this directory
 
-# Symlink ssh config
-ln -s ~/Dropbox/configs/ssh-config.txt ~/.ssh/config
+# Symlink ssh config - not commited to public repo
+ln -sfv ~/Dropbox/Github/mac-setup/ssh/ ~/.ssh
 
