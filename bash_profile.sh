52af8e1e318384a535770eda21ea7201cd5f9514
daniel-wells <daniel-wells@users.noreply.github.com>
2019-07-22 16:21:18 +0100

    add gpg-agent to bash_profile

--- bash_profile.sh
+++ bash_profile.sh
@@ -24,3 +24,12 @@ export HISTSIZE='32768';
 export HISTFILESIZE="${HISTSIZE}";
 # Omit duplicates and commands that begin with a space from history.
 export HISTCONTROL='ignoreboth';
+
+export GPG_TTY=$(tty)
+[ -f ~/.gnupg/gpg-agent-info ] && source ~/.gnupg/gpg-agent-info
+if [ -S "${GPG_AGENT_INFO%%:*}" ]; then
+    export GPG_AGENT_INFO
+else
+    eval $( gpg-agent --daemon --options ~/.gnupg/gpg-agent.conf )
+fi
+
